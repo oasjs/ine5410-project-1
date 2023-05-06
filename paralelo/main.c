c6f1e7e674d40f1e0b213fc434c14586f6c3b2ad
MarcosLacava <marcoslaurean@gmail.com>
2023-05-06 18:21:22 -0300

    fdsa

--- paralelo/main.c
+++ paralelo/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "gol.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
     int size, steps;
     cell_t **prev, **next, **tmp;
     FILE *f;
-    stats_t stats_step = {0, 0, 0, 0};
     stats_t stats_total = {0, 0, 0, 0};
+    int n_threads = 12; 
 
     if (argc != 2)
     {
@@ -38,12 +42,45 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < steps; i++)
     {
-        stats_step = play(prev, next, size);
-        
-        stats_total.borns += stats_step.borns;
-        stats_total.survivals += stats_step.survivals;
-        stats_total.loneliness += stats_step.loneliness;
-        stats_total.overcrowding += stats_step.overcrowding;
+        args_t args_list[n_threads];
+        pthread_t threads[n_threads];
+
+        int step = a_size / max_threads;
+        int remainder = a_size % max_threads;
+        int prev_end = 0;
+
+        for (int t = 0; t < n_threads; t++)
+        {
+            args_list[t] = args;
+
+            args_list[t].start = prev_end;
+            // Executa uma espécie de pidgeonhole sort para que as operações sejam
+            // divididas da maneira mais igualitária possível entre as threads
+            if (remainder > 0) {
+                args_list[t].end = args_list[t].start + step + 1;
+                remainder--;
+            } else {
+                args_list[t].end = args_list[t].start + step;
+            }
+            prev_end = args_list[t].end;
+
+            args_list[t].board = prev;
+            args_list[t].newboard = next;
+            args_list[t].size = size;
+
+            pthread_create(&threads[t], NULL, *play, (void *)&args_list[t])
+        }
+
+        stats_t stats_step;
+
+        for (int t = 0; t < n_threads; t++)
+        {
+            pthread_join(&threads[t], &stats_step)
+            stats_total.borns += stats_step.borns;
+            stats_total.survivals += stats_step.survivals;
+            stats_total.loneliness += stats_step.loneliness;
+            stats_total.overcrowding += stats_step.overcrowding;
+        }
 
 #ifdef DEBUG
         printf("Step %d ----------\n", i + 1);
--- sequencial/gol.h
+++ sequencial/gol.h
@@ -18,6 +18,13 @@
 typedef unsigned char cell_t;
 
 typedef struct {
+    cell_t **board;
+    cell_t **newboard;
+    int size;
+    int start;
+    int end;
+} args_t;
+typedef struct {
     unsigned int borns;
     unsigned int overcrowding;
     unsigned int loneliness;
