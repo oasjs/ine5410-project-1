d8b5eadd93350efccd3e6d2020b7ff633ef3353c
oasjs <oasjatoba1@gmail.com>
2023-05-17 19:42:53 -0300

    Adiciona nova solução, corrigindo acesso aos stats de cada step
    Paralelização continua quebrada.

---
+++ paralelo/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include "gol.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+int main(int argc, char **argv)
+{
+    int size, steps;
+    cell_t **prev, **next, **tmp;
+    FILE *f;
+    stats_t stats_total = {0, 0, 0, 0};
+
+    if (argc != 3)
+    {
+        printf("ERRO! Você deve digitar %s <nome do arquivo do tabuleiro>!\n\n", argv[0]);
+        return 0;
+    }
+
+    if ((f = fopen(argv[1], "r")) == NULL)
+    {
+        printf("ERRO! O arquivo de tabuleiro '%s' não existe!\n\n", argv[1]);
+        return 0;
+    }
+
+    fscanf(f, "%d %d", &size, &steps);
+
+    prev = allocate_board(size);
+    next = allocate_board(size);
+
+    read_file(f, prev, size);
+
+    fclose(f);
+
+#ifdef DEBUG
+    printf("Initial:\n");
+    print_board(prev, size);
+    print_stats(stats_total);
+#endif
+
+    // Define o número de threads
+    int n_threads = atoi(argv[2]);
+    if (n_threads > size)
+        n_threads = size;
+
+    // Inicializa as variáveis para a distribuição de pombal
+    int step = size / n_threads;
+    int remainder = size % n_threads;
+    int prev_end = 0;
+
+    // Inicializa os valores de firstLine, n_lines e size de cada arg.
+    // Isso é feito fora do loop de steps pois esses valores são os mesmos ao
+    // longo de todo o jogo.
+    args_t args[n_threads];
+    for (int i = 0; i < n_threads; i++) 
+    {
+        args[i].firstLine = prev_end;
+        prev_end = args[i].firstLine + step;
+        if (remainder > 0) {
+            ++prev_end;
+            --remainder;
+        }
+        args[i].n_lines = prev_end - args[i].firstLine;
+
+        args[i].size = size;
+    }
+
+    for (int i = 0; i < steps; i++)
+    {
+        pthread_t threads[n_threads];
+
+        for (int j = 0; j < n_threads; j++) 
+        {
+            args[j].board = prev;
+            args[j].newboard = next;
+            // Uso calloc para inicializar os stats em 0
+            args[j].stats = (stats_t*)calloc(1, sizeof(stats_t));
+            pthread_create(&threads[j], NULL, *play, (void*)&args[j]);
+        }
+
+        // Dá join em todas as threads e acumula em stats_step o resultado
+        // de cada thread.
+        stats_t stats_step = {0, 0, 0, 0};
+        for (int j = 0; j < n_threads; j++) {
+            pthread_join(threads[j], NULL);
+            stats_step.borns           += args[j].stats->borns;
+            stats_step.survivals       += args[j].stats->survivals;
+            stats_step.loneliness      += args[j].stats->loneliness;
+            stats_step.overcrowding    += args[j].stats->overcrowding;
+            free(args[j].stats);
+        }   
+        stats_total.borns           += stats_step.borns;
+        stats_total.survivals       += stats_step.survivals;
+        stats_total.loneliness      += stats_step.loneliness;
+        stats_total.overcrowding    += stats_step.overcrowding;
+
+#ifdef DEBUG
+        printf("Step %d ----------\n", i + 1);
+        print_board(next, size);
+        print_stats(stats_step);
+#endif
+        tmp = next;
+        next = prev;
+        prev = tmp;
+    }
+
+#ifdef RESULT
+    printf("Final:\n");
+    print_board(prev, size);
+    print_stats(stats_total);
+#endif
+
+    free_board(prev, size);
+    free_board(next, size);
+}
